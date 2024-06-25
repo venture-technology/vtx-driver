c2b35ab4bc3480073f2e0e5e129daec458256b2c
NFAbricio <fabnunes014@gmail.com>
2024-06-25 00:05:13 -0300

    feat: create controler

--- internal/controller/driver_controller.go
+++ internal/controller/driver_controller.go
@@ -1,1 +1,13 @@
 package controller
+
+import "github.com/venture-technology/vtx-driver/internal/service"
+
+type DriverController struct {
+	driverservice *service.DriverService
+}
+
+func NewDriverController(driverservice *service.DriverService) *DriverController {
+	return &DriverController{
+		driverservice: driverservice,
+	}
+}
