0c1962d7ca65e8863ca63d6c37f0714626fedb22
NFAbricio <fabnunes014@gmail.com>
2024-06-24 23:00:35 -0300

    feat: init connections

--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/venture-technology/vtx-driver/config"
+)
+
+func main() {
+
+	config, err := config.Load("config/config.yaml")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	db, err := sql.Open("postgres", newPostgres(config.Database))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	err = migrate(db, config.Database.Schema)
+	if err != nil {
+		log.Fatalf("failed to execute migrations: %v", err)
+	}
+
+}
+
+func newPostgres(dbconfig config.Database) string {
+	return "user=" + dbconfig.User +
+		" password=" + dbconfig.Password +
+		" dbname=" + dbconfig.Name +
+		" host=" + dbconfig.Host +
+		" port=" + dbconfig.Port +
+		" sslmode=disable"
+}
+
+func migrate(db *sql.DB, filepath string) error {
+	schema, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(string(schema))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
