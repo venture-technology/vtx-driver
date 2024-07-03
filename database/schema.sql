70840f63231b76a4c155cfeca98f58044bf1489d
NFAbricio <fabnunes014@gmail.com>
2024-07-02 22:16:33 -0300

    sql: create table driver

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS drivers (
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    cnh VARCHAR(20) PRIMARY KEY NOT NULL,
+    qrcode VARCHAR(100) NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    complement VARCHAR(10),
+    zip VARCHAR(8) NOT NULL
+);
