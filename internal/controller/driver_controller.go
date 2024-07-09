68a065f8c47688ff23e6fc56f84321f477a7e4c7
NFAbricio <fabnunes014@gmail.com>
2024-07-08 23:55:02 -0300

    refactor: middleware and driver repository

--- internal/controller/driver_controller.go
+++ internal/controller/driver_controller.go
@@ -4,16 +4,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/vtx-driver/internal/middleware"
 	"github.com/venture-technology/vtx-driver/internal/service"
 	"github.com/venture-technology/vtx-driver/models"
 )
-
-type ClaimsDriver struct {
-	CNH string `json:"cnh"`
-	jwt.StandardClaims
-}
 
 type DriverController struct {
 	driverservice *service.DriverService
@@ -27,52 +22,13 @@ func NewDriverController(driverservice *service.DriverService) *DriverController
 
 func (ct *DriverController) RegisterRoutes(router *gin.Engine) {
 
-	// conf := config.Get()
-
-	// driverMiddleware := func(c *gin.Context) {
-
-	// 	secret := []byte(conf.Server.Secret)
-
-	// 	tokenString, err := c.Cookie("token")
-	// 	if err != nil {
-	// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Sem cookie de sessão"})
-	// 		c.Abort()
-	// 		return
-	// 	}
-
-	// 	if tokenString == "" {
-	// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token não fornecido"})
-	// 		c.Abort()
-	// 		return
-	// 	}
-
-	// 	token, err := jwt.ParseWithClaims(tokenString, &ClaimsDriver{}, func(token *jwt.Token) (interface{}, error) {
-	// 		return secret, nil
-	// 	})
-
-	// 	if err != nil {
-	// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
-	// 		c.Abort()
-	// 		return
-	// 	}
-
-	// 	claims, ok := token.Claims.(*ClaimsDriver)
-	// 	if !ok || !token.Valid {
-	// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
-	// 		c.Abort()
-	// 		return
-	// 	}
-
-	// 	c.Set("cnh", claims.CNH)
-	// 	c.Set("isAuthenticated", true)
-	// 	c.Next()
-
-	// }
-
 	api := router.Group("vtx-driver/api/v1")
 
 	api.GET("/ping", ct.Ping) // pingar rota
 	api.POST("/driver", ct.CreateDriver)
+	api.GET("/driver/:cnh")
+	api.PATCH("/driver/:cnh", middleware.DriverMiddleware())
+	api.DELETE("/driver/:cnh", middleware.DriverMiddleware())
 
 }
 
--- internal/repository/driver_repository.go
+++ internal/repository/driver_repository.go
@@ -26,28 +26,28 @@ func NewDriverRepository(conn *sql.DB) *DriverRepository {
 	}
 }
 
-func (d *DriverRepository) CreateDriver(ctx context.Context, driver *models.Driver) error {
+func (dr *DriverRepository) CreateDriver(ctx context.Context, driver *models.Driver) error {
 	sqlQuery := `INSERT INTO drivers (qrcode, name, email, password, cpf, cnh, street, number, zip, complement) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err := d.db.Exec(sqlQuery, driver.QrCode, driver.Name, driver.Email, driver.Password, driver.CPF, driver.CNH, driver.Street, driver.Number, driver.ZIP, driver.Complement)
+	_, err := dr.db.Exec(sqlQuery, driver.QrCode, driver.Name, driver.Email, driver.Password, driver.CPF, driver.CNH, driver.Street, driver.Number, driver.ZIP, driver.Complement)
 	return err
 }
 
-func (d *DriverRepository) DeleteDriver(ctx context.Context, cnh *string) error {
+func (dr *DriverRepository) DeleteDriver(ctx context.Context, cnh *string) error {
 	return nil
 }
 
-func (d *DriverRepository) UpdateDriver(ctx context.Context, driver *models.Driver) error {
+func (dr *DriverRepository) UpdateDriver(ctx context.Context, driver *models.Driver) error {
 	return nil
 }
 
-func (d *DriverRepository) GetDriver(ctx context.Context, cnh *string) (*models.Driver, error) {
+func (dr *DriverRepository) GetDriver(ctx context.Context, cnh *string) (*models.Driver, error) {
 	return nil, nil
 }
 
-func (d *DriverRepository) FindAllDrivers(ctx context.Context) ([]models.Driver, error) {
+func (dr *DriverRepository) FindAllDrivers(ctx context.Context) ([]models.Driver, error) {
 	return nil, nil
 }
 
-func (d *DriverRepository) AuthDriver(ctx context.Context, driver *models.Driver) (*models.Driver, error) {
+func (dr *DriverRepository) AuthDriver(ctx context.Context, driver *models.Driver) (*models.Driver, error) {
 	return nil, nil
 }
