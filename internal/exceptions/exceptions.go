bfd4a569a5a69e5b1028b37d490184051ff01306
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-15 15:02:54 -0300

    feat: type exception

--- internal/exceptions/exceptions.go
+++ internal/exceptions/exceptions.go
@@ -17,3 +17,9 @@ func InternalServerResponseError(err error, msg string) gin.H {
 		"error": fmt.Sprintf("internal server error - %s: %s", msg, err.Error()),
 	}
 }
+
+func TypeServerResponseError(msg string) gin.H {
+	return gin.H{
+		"error": fmt.Sprintf("type error: %s", msg),
+	}
+}
